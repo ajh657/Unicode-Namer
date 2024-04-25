b2e444a9c0ec419cea0730ad1d4023c05a694df6
ajh657 <aahoo01@gmail.com>
2024-04-25 20:45:35 +0300

    Saving commit

---
+++ UnicodeNamer/UnicodeNamer/EmojiRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace UnicodeNamer;
+
+public record EmojiRecord
+{
+    public required string Emoji { get; init; }
+    public required string Name { get; init; }
+    public required string Shortname { get; init; }
+    public required string Unicode { get; init; }
+    public required string Html { get; init; }
+    public required string Category { get; init; }
+    public required string Order { get; init; }
+}
---
+++ UnicodeNamer/UnicodeNamer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace UnicodeNamer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var files = Directory.GetFiles(args[0]);
+        var records = JsonConvert.DeserializeObject<List<EmojiRecord>>(File.ReadAllText("./emojis.json"));
+        
+        if (records == null)
+        {
+            return;
+        }
+        
+        foreach (var file in files)
+        {
+            var info = new FileInfo(file);
+            var extension = info.Extension;
+            var emojis = Path.GetFileNameWithoutExtension(file).Split(['-', '_']);
+            var name = string.Join('_', emojis.Select(e => records.FirstOrDefault(r => r.Unicode.Equals(e.TrimStart('u'), StringComparison.InvariantCultureIgnoreCase))?.Name.Replace(" ", string.Empty ) ?? e));
+            
+            if (info.DirectoryName == null)
+            {
+                continue;
+            }
+            
+            var newName = Path.Combine(info.DirectoryName, name + extension);
+            File.Move(file, newName);
+        }
+    }
+}
